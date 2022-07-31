b5569fe58c7c9dd8f2e8629ed0d5685cbceb5db9
Matthew Pottinger <matthewpottinger@gmail.com>
2022-07-31 12:52:53 -0600

    Initial commit

---
+++ receiver.py
@@ -0,0 +1,27 @@
+# send POST request with jpg from opencv webcam image converted to Base64 text to http server,
+# and then a GET request to the same server to get the image to display
+
+import requests
+import sys
+import cv2
+import numpy as np
+import base64
+
+url = "http://localhost:8000/dw1"
+
+while True:
+    # send GET request to the server
+    r = requests.get(url)
+    # decode Base64 text to image
+    text = r.text
+    jpeg_decoded = base64.b64decode(r.text)
+    # convert image to numpy array
+    jpeg_decoded = np.fromstring(jpeg_decoded, dtype=np.uint8)
+    # convert numpy array to image
+    jpeg_decoded = cv2.imdecode(jpeg_decoded, cv2.IMREAD_COLOR)
+    # display image
+    cv2.imshow('frame', jpeg_decoded)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
---
+++ sender.py
@@ -0,0 +1,22 @@
+# send POST request with jpg from opencv webcam image converted to Base64 text to http server,
+# and then a GET request to the same server to get the image to display
+
+import requests
+import sys
+import cv2
+import numpy as np
+import base64
+
+url = "http://localhost:8000/dw1"
+
+# read image from webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    # convert image to Base64 text and post to server
+    ret, jpeg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
+    jpeg_text = base64.b64encode(jpeg)
+    r = requests.post(url, data=jpeg_text)
+
+
