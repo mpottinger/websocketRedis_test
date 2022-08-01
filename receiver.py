97a389adb2dc229ab8d27574acc06071b595d9eb
matthewpottinger <matthewpottinger@gmail.com>
2022-07-31 18:19:59 -0600

    .

--- receiver.py
+++ receiver.py
@@ -1,4 +1,4 @@
-# send POST request with jpg from opencv webcam image converted to Base64 text to http server,
+# send POST request with jpg from opencv webcam image to http server,
 # and then a GET request to the same server to get the image to display
 
 import requests
@@ -7,16 +7,18 @@ import cv2
 import numpy as np
 import base64
 
-url = "http://75.152.195.28:8000/dw1"
+#url = "http://75.152.195.28:8000/dw1"
+url = "http://localhost:8000/dw1"
 
 while True:
     # send GET request to the server
     r = requests.get(url)
     # decode Base64 text to image
-    text = r.text
-    jpeg_decoded = base64.b64decode(r.text)
+    #jpeg_decoded = base64.b64decode(r.text)
+    jpeg_decoded = r.content
+    # add html image tag to the decoded text
     # convert image to numpy array
-    jpeg_decoded = np.fromstring(jpeg_decoded, dtype=np.uint8)
+    jpeg_decoded = np.frombuffer(jpeg_decoded, dtype=np.uint8)
     # convert numpy array to image
     jpeg_decoded = cv2.imdecode(jpeg_decoded, cv2.IMREAD_COLOR)
     # display image
