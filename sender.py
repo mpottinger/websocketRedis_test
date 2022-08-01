aaa4c958d8bfcb3dade69ef93fb3ffc27d0d8214
matthewpottinger <matthewpottinger@gmail.com>
2022-07-31 22:06:05 -0600

    .

--- sender.py
+++ sender.py
@@ -7,9 +7,11 @@ import cv2
 import numpy as np
 import base64
 
-url = "http://137.186.157.198:8000/dw1"
+url = "http://localhost:8000/dw1"
 content_type = 'image/jpeg'
 #content_type = 'application/octet-stream'
+#content_type = 'video/x-motion-jpeg'
+
 # read image from webcam, set resolution to 640x480
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -17,11 +19,13 @@ cap.set(4, 480)
 
 while True:
     ret, frame = cap.read()
-    # convert image to Base64 text and post to server
-    ret, jpeg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
-    #jpeg_text = base64.b64encode(jpeg)
+    # make sure we have a valid frame
+    if ret:
+        # convert image to Base64 text and post to server
+        ret, jpeg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
+        #jpeg_text = base64.b64encode(jpeg)
 
-    # send as binary data
-    r = requests.post(url, data=jpeg.tobytes(), headers={'Content-Type': content_type})
+        # send as binary data
+        r = requests.post(url, data=jpeg.tobytes(), headers={'Content-Type': content_type})
 
 
