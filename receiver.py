cbb44536f2b9fe3951155c6e02bdea3c63d1b2a4
Matthew Pottinger <matthewpottinger@gmail.com>
2022-08-04 20:07:48 -0600

    .

--- receiver.py
+++ receiver.py
@@ -1,31 +1,39 @@
 # send POST request with jpg from opencv webcam image to http server,
 # and then a GET request to the same server to get the image to display
 
-import requests
+import urllib3
 import sys
 import cv2
 import numpy as np
 import base64
 
-url = "http://137.186.157.198:8000/dw1"
-#url = "http://localhost:8000/dw1"
+#url = "http://34.106.72.196:80/dw1"
+url = "http://localhost:8000/dw1"
 
+http = urllib3.PoolManager()
 while True:
-    # send GET request to the server, receiving binary data
-    r = requests.get(url)
-    print("content type:", r.headers['Content-Type'])
-    # convert binary data to numpy array
-    img = np.frombuffer(r.content, dtype=np.uint8)
-    #print(r.content)
-    #print("Received content of size:", len(r.content))
-    #print("np frombuffer shape: ", img.shape)
-    # decode numpy array to opencv image
-    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    # display image
-    cv2.imshow('image', img)
-    # wait for key press
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
+    # send POP request to the server, receiving binary data and deleting it from the server
+    # the video sender will only upload the data if the key is not found on the server, saving bandwidth
+    # "Wait" header is set to true, and the server will wait for the data to be uploaded before sending the next request
+    # "Delete header is set to true, and the server will delete the data after it is downloaded
+    r = http.request('GET', url, headers={'Wait': 'true', 'Delete': 'true'})
+    # check if server response was successful
+    if r.status == 200:
+        #print("content type:", r.headers['Content-Type'])
+        # convert binary data to numpy array
+        img = np.frombuffer(r.data, dtype=np.uint8)
+        #print(r.content)
+        #print("Received content of size:", len(r.content))
+        #print("np frombuffer shape: ", img.shape)
+        # decode numpy array to opencv image
+        img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+        # display image
+        cv2.imshow('image', img)
+        # wait for key press
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+    else:
+        print("server returned:", r.status)
 
 
--- sender.py
+++ sender.py
@@ -1,27 +1,37 @@
 # send POST request with jpg from opencv webcam image converted to Base64 text to http server,
 # and then a GET request to the same server to get the image to display
+import time
 
-import requests
+import urllib3
 import sys
 import cv2
 import numpy as np
 import base64
 
-url = "http://137.186.157.198:8000/dw1"
+#url = "http://34.106.72.196:80/dw1"
+url = "http://localhost:8000/dw1"
 content_type = 'image/jpeg'
 #content_type = 'application/octet-stream'
 # read image from webcam, set resolution to 640x480
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-
+cap.set(3, 320)
+cap.set(4, 240)
+http = urllib3.PoolManager()
 while True:
     ret, frame = cap.read()
     # convert image to Base64 text and post to server
     ret, jpeg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
     #jpeg_text = base64.b64encode(jpeg)
-
+    jpeg_bytes = jpeg.tobytes()
     # send as binary data
-    r = requests.post(url, data=jpeg.tobytes(), headers={'Content-Type': content_type})
+    # check if there is unread data on the server, header sent to server will contain 'Exists', and server will return OK if there is unread data
+    r = http.request('GET', url, body="", headers={'Exists': 'true'})
+    # post the data if key is not found on the server
+    if r.status == 404:
+        # data wasn't found on the server, post it and print time stamp
+        print("sending data at:", time.time())
+        r = http.request('POST', url, body=jpeg_bytes, headers={'Content-Type': content_type})
+        # print the status code of the response
+    print('server returned:', r.status)
 
 
