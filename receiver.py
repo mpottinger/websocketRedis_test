f77b94a2401b8bcaa51a7df4f466873fd4237175
Matthew Pottinger <matthewpottinger@gmail.com>
2022-08-21 03:56:10 -0600

    .

--- receiver.py
+++ receiver.py
@@ -2,40 +2,50 @@
 # and then a GET request to the same server to get the image to display
 import time
 
-import requests
 import sys
 import cv2
 import numpy as np
 import base64
+import websocket
+import json
 
-#url = "http://34.106.72.196:80/dw1"
-url = "http://localhost:8000/dw1"
-s = requests.Session()
+class ClientRequest:
+    def __init__(self, method, timeout, key, size):
+        self.method = method
+        self.timeout = timeout
+        self.key = key
+        self.size = size
+
+#url = "ws://34.106.72.196:80/dw1"
+url = "ws://localhost:8000/dw1"
+s = websocket.WebSocket()
 frame_count = 1
 start = time.time()
+# connect to websocket server
+s.connect(url)
 while True:
-    # send Get request to the server, this will pop an item from the queue of a given key
-    # the video sender will only upload the data if the queue is empty for a given key, saving bandwidth
-    print("getting data from server.")
-    r = s.get(url)
-    # check if server response was successful
-    print("server returned:", r.status_code)
-    if r.status_code == 200:
-        # calculate FPS
-        fps = (time.time() - start) / frame_count
-        fps = 1 / fps
-        frame_count += 1
-        print("FPS: %.2f" % fps)
+    # read message from websocket server
+    msg = "CLIENT"
+    req = ClientRequest("GET", 10, "image", len(msg))
+    req = json.dumps(req.__dict__)
+    s.send(req + msg,websocket.ABNF.OPCODE_BINARY)
+    msg = s.recv()
+    if(msg == "TIMEOUT"):
+        print("TIMEOUT")
+        continue
+    # convert message to numpy array (bytes)
+    img = np.frombuffer(msg, dtype=np.uint8)
+    # # convert numpy array to opencv image
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    # # display image
+    cv2.imshow('image', img)
+    # # press q to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+         break
+    # # print frame count and time per frame
+    frame_count += 1
+    start = time.time()
 
-        # convert binary data to numpy array
-        img = np.frombuffer(r.content, dtype=np.uint8)
-        # decode numpy array to opencv image
-        img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-        # display image
-        cv2.imshow('image', img)
-        # wait for key press
-        k = cv2.waitKey(1) & 0xFF
-        if k == 27:
-                break
+
 
 
--- sender.py
+++ sender.py
@@ -2,20 +2,39 @@
 # and then a GET request to the same server to get the image to display
 import time
 
-import requests
+import websocket
 import sys
 import cv2
 import numpy as np
-import base64
+import json
 
-#url = "http://34.106.72.196:80/dw1"
-url = "http://localhost:8000/dw1"
+#url = "ws://34.106.72.196:80/dw1"
+url = "ws://localhost:8000/dw1"
 
-content_type = 'image/jpeg'
-# header for POST request
 # initialize video capture
 cap = cv2.VideoCapture(0)
-s = requests.Session()
+
+# web socket with binary data enabled.
+s = websocket.WebSocket(skip_utf8_validation=True)
+# connect to websocket server
+s.connect(url)
+
+# this C# class is defined on the server, here we implement the same class
+#public record ClientRequest
+#    {
+#        // structured almost like a http request
+#        public string Method { get; set; }
+#        public int Timeout { get; set; }
+#        public string Key { get; set; }
+#        public int Size { get; set; }
+#    }
+
+class ClientRequest:
+    def __init__(self, method, timeout, key, size):
+        self.method = method
+        self.timeout = timeout
+        self.key = key
+        self.size = size
 
 while True:
     # read image from webcam, set resolution to 640x480
@@ -24,9 +43,23 @@ while True:
     img = cv2.imencode('.jpg', frame)[1]
     # convert jpeg image to raw byte array
     img = img.tobytes()
-    r = s.put(url, data=img, headers={'Content-Type': content_type})
+    # create ClientRequest object
+    req = ClientRequest("PUT", 10, "image", len(img))
+    # convert ClientRequest object to json text
+    req = json.dumps(req.__dict__)
+    req_bytes = bytes(req, 'utf-8')
+    # concatenate json text and jpeg image
+    msg = req_bytes + img
+    # send message to websocket server OPCODE_BINARY
+    s.send(msg, websocket.ABNF.OPCODE_BINARY)
 
-    # check if server response was successful
-    print("server returned:", r.status_code, r.text)
+
+    # s.send("test")
+    # check if message says timeout
+    msg = s.recv()
+    print(msg)
+    if msg == "timeout":
+        print("timeout")
+
 
 
